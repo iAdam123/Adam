bd3e19563d96cd2fa73742911750d7600b4099ca
iAdam123 <130289237+iAdam123@users.noreply.github.com>
2026-05-16 04:34:05 +0300

    Add Car model class with properties

---
+++ Models/Car.cs
@@ -0,0 +1,21 @@
+namespace AdamGarage.Models
+{
+    public class Car
+    {
+              public int Car_Id { get; set; }
+              public string Car_Model { get; set; }
+              public string Car_Type { get; set; }
+              public string Car_Name { get; set; }
+              public string Plate_Number { get; set; }
+              public string Engine { get; set; }
+              public string Color { get; set; }
+              public int Year { get; set; }
+              public string Status { get; set; }
+              public int Seats { get; set; }
+              public int Horses { get; set; }
+              public string Transmission { get; set; }
+              public string Fuel_Type { get; set; }
+              public double Mileage { get; set; }
+              public decimal Price { get; set; }
+    }
+}
